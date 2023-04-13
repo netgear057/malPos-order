accbdc97ed91d1db86f6f75b9e27ff9518b7b44a
11khan <you@example.com>
2023-04-14 02:04:27 +0500

    design update

---
+++ src/components/cards/ImageCards.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Box } from '../elements'
+import {Text} from '../elements'
+export default function ImageCards({className,Imgsrc,alt,productTitle}) {
+  return (
+    <div>
+        <Box className={'image-card-box '}>
+            <img src={Imgsrc} alt='img'/>
+            <Text className={'image-card-overlay-text f-13'}  as="span">{productTitle}</Text>
+        </Box>
+    </div>
+  )
+}
