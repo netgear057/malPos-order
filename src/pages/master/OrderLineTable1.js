942b6a76efc9ef19093e8833fa91e8daf743a165
[Shahid Raza] <[netgear057@gmail.com]>
2023-04-18 03:28:34 +0500

    ids

--- src/pages/master/OrderLineTable1.js
+++ src/pages/master/OrderLineTable1.js
@@ -7,88 +7,82 @@ import { Box, Text } from "../../components/elements";
 export default function OrderLineTable1() {
   return (
     <div>
-         <Col md={12}>
-            <CardLayout>
-              <Box className={"order-line-tables-wrapper"}>
-                <Box className={"order-line-tables"}>
-                  <Row>
-                    <Col md={3}>
-                      <Box className={"child-one-box-container"}>
-                        <Box className={"child-one-box"}>
-                          <h6>Table 1</h6>
-                          <br />
-                          <Text as="span">4</Text>
-                        </Box>
-                        <Link to={"/my-products"} className={"child-one-box"}>
-                          <h6>Table 1</h6>
-                          <br />
-                          <Text as="span">4</Text>
-                        </Link>
-                        <Link to={"/my-products"} className={"child-one-box"}>
-                          <h6>Table 1</h6>
-                          <br />
-                          <Text as="span">4</Text>
-                        </Link>
-                        <Link to={"/my-products"} className={"child-one-box"}>
-                          <h6>Table 1</h6>
-                          <br />
-                          <Text as="span">4</Text>
-                        </Link>
-                      </Box>
-                    </Col>
-                    <Col md={3}>
-                      <Link
-                        to={"/my-products"}
-                        className={"child-two-box-container"}
-                      >
-                        <Box className={"child-two-box"}>
-                          <h6>Table 1</h6>
-                          <br />
-                          <Text as="span">4</Text>
-                        </Box>
-                      </Link>
-                    </Col>
-                    <Col md={3}>
-                      <Box className={"child-thr-box-container"}>
-                        <Link to={"/my-products"} className={"child-thr-box"}>
-                          <h6>Table 1</h6>
-                          <br />
-                          <Text as="span">4</Text>
-                        </Link>
-                        <Link to={"/my-products"} className={"child-thr-box"}>
-                          <h6>Table 1</h6>
-                          <br />
-                          <Text as="span">4</Text>
-                        </Link>
-                      </Box>
-                    </Col>
-                    <Col md={3}>
-                      <Box className={"child-one-box-container"}>
-                        <Link to={"/my-products"} className={"child-one-box"}>
-                          <h6>Table 1</h6>
-                          <br />
-                          <Text as="span">4</Text>
-                        </Link>
-                        <Link
-                          to={"/my-products"}
-                          className={"child-one-box child-four-box"}
-                        >
-                          <h6>Table 1</h6>
-                          <br />
-                          <Text as="span">4</Text>
-                        </Link>
-                        <Link to={"/my-products"} className={"child-one-box"}>
-                          <h6>Table 1</h6>
-                          <br />
-                          <Text as="span">4</Text>
-                        </Link>
-                      </Box>
-                    </Col>
-                  </Row>
-                </Box>
-              </Box>
-            </CardLayout>
-          </Col>
+      <Col md={12}>
+        <CardLayout>
+          <Box className={"order-line-tables-wrapper"}>
+            <Box className={"order-line-tables"}>
+              <Row>
+                <Col md={3}>
+                  <Box className={"child-one-box-container"}>
+                    <Box className={"child-one-box"}>
+                      <h6>Table 1</h6>
+                      <br />
+                      <Text as="span">4</Text>
+                    </Box>
+                    <Link to={""} className={"child-one-box"}>
+                      <h6>Table 1</h6>
+                      <br />
+                      <Text as="span">4</Text>
+                    </Link>
+                    <Link to={""} className={"child-one-box"}>
+                      <h6>Table 1</h6>
+                      <br />
+                      <Text as="span">4</Text>
+                    </Link>
+                    <Link to={""} className={"child-one-box"}>
+                      <h6>Table 1</h6>
+                      <br />
+                      <Text as="span">4</Text>
+                    </Link>
+                  </Box>
+                </Col>
+                <Col md={3}>
+                  <Link to={""} className={"child-two-box-container"}>
+                    <Box className={"child-two-box"}>
+                      <h6>Table 1</h6>
+                      <br />
+                      <Text as="span">4</Text>
+                    </Box>
+                  </Link>
+                </Col>
+                <Col md={3}>
+                  <Box className={"child-thr-box-container"}>
+                    <Link to={""} className={"child-thr-box"}>
+                      <h6>Table 1</h6>
+                      <br />
+                      <Text as="span">4</Text>
+                    </Link>
+                    <Link to={""} className={"child-thr-box"}>
+                      <h6>Table 1</h6>
+                      <br />
+                      <Text as="span">4</Text>
+                    </Link>
+                  </Box>
+                </Col>
+                <Col md={3}>
+                  <Box className={"child-one-box-container"}>
+                    <Link to={""} className={"child-one-box"}>
+                      <h6>Table 1</h6>
+                      <br />
+                      <Text as="span">4</Text>
+                    </Link>
+                    <Link to={""} className={"child-one-box child-four-box"}>
+                      <h6>Table 1</h6>
+                      <br />
+                      <Text as="span">4</Text>
+                    </Link>
+                    <Link to={""} className={"child-one-box"}>
+                      <h6>Table 1</h6>
+                      <br />
+                      <Text as="span">4</Text>
+                    </Link>
+                  </Box>
+                </Col>
+              </Row>
+            </Box>
+          </Box>
+        </CardLayout>
+      </Col>
     </div>
-  )
+  );
 }
